cca60fa671157bf0b8d9f954ac41d995f2f95ade
rabbyhasan <rabbyhasan4594@gmail.com>
2023-07-23 23:20:20 +0600

    get for colleges

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+require('dotenv').config();
 const port = process.env.PORT || 5000
 
 
@@ -13,10 +13,55 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+//mongoDb
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2yyawyx.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    client.connect();
+        const collegesCollection = client.db('bookMyCampus').collection('colleges');
+
+
+
+        app.get('/colleges', async (req, res) => {
+            const result = await collegesCollection.find({}).toArray();
+            res.send(result);
+        })
+
+       
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
-    res.send('Dancing start')
+    res.send('Booking start')
 })
 app.listen(port, () => {
-    console.log(`Dancing is running on port ${port}`)
+    console.log(`Booking going on ${port}`)
 })
